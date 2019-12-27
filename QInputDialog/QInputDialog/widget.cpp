69d16e7e55b399ca7ff94523aa81b88bc7beb235
Emrah <emrahsarboz@gmail.com>
2019-12-26 23:58:04 -0700

    QInputDialog

---
+++ QInputDialog/QInputDialog/widget.cpp
@@ -0,0 +1,27 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QInputDialog>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    bool ok;
+    double d = QInputDialog::getDouble(this, tr("QInputDialog::getDouble()"),
+                                       tr("Amount: "), 37.56, -10000, 10000, 2, &ok);
+
+    if(ok)
+        qDebug()<<"Your double value is : "<<QString::number(d);
+}
