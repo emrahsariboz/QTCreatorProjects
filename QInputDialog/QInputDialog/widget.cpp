393077d44a290e26f36fb5c86334912e807ea1d1
Emrah <emrahsarboz@gmail.com>
2019-12-27 00:01:01 -0700

    QInputDialog

--- QInputDialog/QInputDialog/widget.cpp
+++ QInputDialog/QInputDialog/widget.cpp
@@ -18,10 +18,21 @@ Widget::~Widget()
 
 void Widget::on_pushButton_clicked()
 {
+    /*
     bool ok;
     double d = QInputDialog::getDouble(this, tr("QInputDialog::getDouble()"),
                                        tr("Amount: "), 37.56, -10000, 10000, 2, &ok);
 
-    if(ok)
-        qDebug()<<"Your double value is : "<<QString::number(d);
+                                       */
+
+    QStringList items;
+    items<<tr("Spring")<<tr("Summer")<<tr("Fall")<<tr("Winter");
+
+    bool ok;
+
+    QString item = QInputDialog::getItem(this, tr("QInputDialog::getItem()"),
+                                         tr("Season:"), items, 0, false, &ok);
+
+    if(ok && !item.isEmpty())
+        qDebug()<<"Your chosen item is : "<<item;
 }
