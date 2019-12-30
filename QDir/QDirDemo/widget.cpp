6c608a486e221f33f40df81b1dc9438831f6fb0f
Emrah <emrahsarboz@gmail.com>
2019-12-29 23:46:10 -0700

    QDir List

--- QDir/QDirDemo/widget.cpp
+++ QDir/QDirDemo/widget.cpp
@@ -49,11 +49,45 @@ void Widget::on_createDirButton_clicked()
 void Widget::on_isValidDirButton_clicked()
 {
 
+    QString dirPath = ui->lineEditDir->text();
+    QDir dir(dirPath);
+    if(dir.exists())
+        QMessageBox::information(this, "Message", "Directory already exists!");
+    else
+        QMessageBox::information(this, "Message", "New directory is not exist!");
 
 }
 
 void Widget::on_dirorFilebutton_clicked()
 {
+
+    ui->listWidget->clear();
+
+    //Check if dir or file
+
+    QString dirPath = ui->lineEditDir->text();
+
+    if(dirPath.isEmpty())
+        return;
+
+    QDir dir(dirPath);
+
+    //Get the list of files and directories in the folder
+
+    QList<QFileInfo> filesList = dir.entryInfoList();
+
+    for(int i=0; i<filesList.size(); i++){
+        QString prefix;
+        if(filesList.at(i).isFile()){
+            prefix = "File :";
+        }
+
+        if(filesList.at(i).isDir()){
+            prefix = "Directory :";
+        }
+
+        ui->listWidget->addItem(prefix + filesList.at(i).absoluteFilePath());
+    }
 
 
 }
