f60edd310c04c315be43c3afaf7286acd063be8d
Emrah <emrahsarboz@gmail.com>
2020-01-01 16:46:44 -0700

    Roles

--- CustomModel/CustomModel/customtablemodel.cpp
+++ CustomModel/CustomModel/customtablemodel.cpp
@@ -1,5 +1,6 @@
 #include "customtablemodel.h"
-
+#include <QFont>
+#include <QBrush>
 CustomTableModel::CustomTableModel(QObject *parent) : QAbstractTableModel()
 {
 
@@ -17,12 +18,50 @@ int CustomTableModel::columnCount(const QModelIndex &parent) const
 
 QVariant CustomTableModel::data(const QModelIndex &index, int role) const
 {
+    /*
     if(role == Qt::DisplayRole){
         QString value = QString("Row %1, Col %2")
                 .arg(index.row() + 1)
                 .arg(index.column()+1);
 
         return value;
+    }
+    */
+    int row = index.row();
+    int col = index.column();
+
+    switch (role) {
+        case Qt::DisplayRole:
+            if(row == 0 && col == 1) return QString("<-- left");
+            if(row == 1 && col == 1) return QString("right-->");
+
+        return QString("Row %1, Col %2")
+                .arg(row)
+                .arg(col);
+
+
+        case Qt::FontRole:
+            if(row == 0 && col == 0){
+                 QFont font;
+                 font.setBold(true);
+                 return font;
+            }
+        break;
+
+        case Qt::BackgroundRole:
+        if(row == 1 && col == 2){
+            QBrush background(Qt::yellow);
+            return background;
+        }
+
+        break;
+
+    case Qt::TextAlignmentRole:
+        if(row == 1 && col == 1){
+            return Qt::AlignRight;
+        }
+
+        break;
     }
     return QVariant();
 }
